ada5e8ecaf2911c99096b62ac385dda4e0fd432f
Priyanshu Yadav <yadavpriyanshu232101@gmail.com>
2026-06-15 10:02:52 +0530

    Added week3 day1 soluition

---
+++ week3/day1/quest1.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        if(head == NULL || head->next == NULL)
+            return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    head->next->next->next->next = head->next;
+
+    Solution obj;
+
+    if(obj.hasCycle(head))
+        cout << "Cycle exists";
+    else
+        cout << "No cycle";
+
+    return 0;
+}
